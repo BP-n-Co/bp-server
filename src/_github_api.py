e9ce05dc52254d2d0dde81dc20f68a0aab53bbe7
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-05-11 11:28:27 +0900

    [BP-47] Alix/bp 47 add better logging especially in exception handling add the (#26)
    * proper logger in mysql client
    
    * better except to service
    
    * add logging in github client
    
    * typos

--- src/_github_api.py
+++ src/_github_api.py
@@ -3,7 +3,7 @@ from logging import Logger
 
 from requests import Session
 
-from src._config import GITHUB_TOKEN
+from src._config import GITHUB_TOKEN, base_logger
 from src.models import GitUser, Repository
 
 
@@ -12,9 +12,14 @@ class GithubRequestException(Exception):
         super().__init__(f"Error when requesting Github Api, {detail}")
 
 
+class GithubWrongAttributesException(Exception):
+    def __init__(self, detail: str | None = None) -> None:
+        super().__init__(f"wrong attributes when requesting github, {detail}")
+
+
 class GithubClient:
     def __init__(self, logger: Logger | None = None, token: str | None = None) -> None:
-        self.logger = logger
+        self.logger = logger if logger else base_logger
         self.session = Session()
         self.token = token if token else GITHUB_TOKEN
         self.date_format = "%Y-%m-%dT%H:%M:%SZ"
@@ -28,19 +33,19 @@ class GithubClient:
             url="https://api.github.com/graphql", headers=headers, json={"query": query}
         )
         if not resp.status_code == 200:
-            raise GithubRequestException(
-                detail=f"could not get response from Github {resp.status_code=}."
-            )
+            message = f"could not get response from Github {resp.status_code=}."
+            self.logger.error(message)
+            raise GithubRequestException(detail=message)
         try:
             resp_dict = resp.json()
         except Exception as e:
-            raise GithubRequestException(
-                detail=f"could not serialized Github response : {type(e)=}, {str(e)=}."
-            )
+            message = f"could not serialized Github response : {type(e)=}, {str(e)=}."
+            self.logger.warning(message)
+            raise GithubRequestException(detail=message)
         if not isinstance(resp_dict, dict) or "data" not in resp_dict:
-            raise GithubRequestException(
-                detail=f"got response without data : {str(resp_dict)=}"
-            )
+            message = f"got response without data : {str(resp_dict)=}"
+            self.logger.warning(message)
+            raise GithubRequestException(detail=message)
         return resp_dict["data"]
 
     def get_user_info(self, id: str) -> GitUser:
@@ -56,14 +61,11 @@ class GithubClient:
                 }}
             }}
         """
-        try:
-            user_info = self.graphql_post(query=query)["node"]
-        except GithubRequestException as e:
-            raise e
+        user_info = self.graphql_post(query=query)["node"]
         if not user_info:
-            raise GithubRequestException(
-                detail=f"could not retreive any user info for {id=}"
-            )
+            message = f"could not retreive any user info for {id=}"
+            self.logger.warning(message)
+            raise GithubWrongAttributesException(detail=message)
         user = GitUser(
             id=id,
             avatarUrl=user_info["avatarUrl"],
@@ -86,14 +88,11 @@ class GithubClient:
             }}
         }}
         """
-        try:
-            repo_info = self.graphql_post(query=query)["repository"]
-        except GithubRequestException as e:
-            raise e
+        repo_info = self.graphql_post(query=query)["repository"]
         if not repo_info:
-            raise GithubRequestException(
-                detail=f"could not retreive any repository info for {name=}, {owner=}"
-            )
+            message = f"could not retreive any repository info for {name=}, {owner=}"
+            self.logger.warning(message)
+            raise GithubWrongAttributesException(detail=message)
         repo = Repository(
             id=repo_info["id"],
             name=name,
