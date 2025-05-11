8e7a78816e550791a8a38b52782f5cf05b634d8c
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-05-11 11:28:27 +0900

    [BP-47] Alix/bp 47 add better logging especially in exception handling add the (#26)
    * proper logger in mysql client
    
    * better except to service
    
    * add logging in github client
    
    * typos

--- src/api/v1/repository/service.py
+++ src/api/v1/repository/service.py
@@ -3,16 +3,21 @@ from pymysql.err import IntegrityError
 from src._config import base_logger
 from src._database_pymysql import (
     MysqlClient,
-    NoConnectionError,
-    NoUpdateValuesError,
-    NoValueInsertionError,
+    MySqlNoConnectionError,
+    MySqlNoUpdateValuesError,
+    MySqlNoValueInsertionError,
+    MySqlWrongQueryError,
 )
 from src._exceptions import (
     AlreadyExistsException,
     NotFoundException,
     WrongAttributesException,
 )
-from src._github_api import GithubClient, GithubRequestException
+from src._github_api import (
+    GithubClient,
+    GithubRequestException,
+    GithubWrongAttributesException,
+)
 from src.models import GitUser, Repository
 
 
@@ -27,7 +32,7 @@ def add_repository(name: str, owner: str) -> Repository:
     # 1. Call Github to get the info about the repo (nodeId)
     try:
         repo = github_client.get_repository_info(name=name, owner=owner)
-    except GithubRequestException as e:
+    except (GithubRequestException, GithubWrongAttributesException) as e:
         quit()
         raise e
 
@@ -35,18 +40,16 @@ def add_repository(name: str, owner: str) -> Repository:
     # 2.1 Get user info
     try:
         github_user = github_client.get_user_info(id=str(repo.ownerId))
-    except GithubRequestException as e:
+    except (GithubRequestException, GithubWrongAttributesException) as e:
         quit()
         raise e
 
     # 2.2 Add user if not present
     try:
-        existence = mysql_client.select(
-            table_name=GitUser.__tablename__,
-            select_col=["id"],
-            cond_eq={"id": str(github_user.id)},
+        existence = mysql_client.select_by_id(
+            table_name=GitUser.__tablename__, id=str(github_user.id)
         )
-    except NoConnectionError as e:
+    except (MySqlNoConnectionError, MySqlWrongQueryError) as e:
         quit()
         raise e
 
@@ -55,7 +58,11 @@ def add_repository(name: str, owner: str) -> Repository:
             mysql_client.insert_one(
                 table_name=GitUser.__tablename__, values=github_user.to_dict()
             )
-        except (NoValueInsertionError, NoConnectionError) as e:
+        except (
+            MySqlNoValueInsertionError,
+            MySqlNoConnectionError,
+            MySqlWrongQueryError,
+        ) as e:
             quit()
             raise e
 
@@ -65,12 +72,16 @@ def add_repository(name: str, owner: str) -> Repository:
         mysql_client.insert_one(
             table_name=Repository.__tablename__, values=repo.to_dict()
         )
-    except (NoValueInsertionError, NoConnectionError) as e:
+    except (
+        MySqlNoValueInsertionError,
+        MySqlNoConnectionError,
+        MySqlWrongQueryError,
+    ) as e:
         quit()
         raise e
     except IntegrityError as e:
         quit()
-        raise AlreadyExistsException(table_name=Repository.__tablename__, error=e)
+        raise AlreadyExistsException(table_name=Repository.__tablename__, detail=str(e))
 
     quit()
     # 4. Return created one
