79ab811182168b0b57b23d2ec11cc58cd7a72961
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-05-14 23:15:43 +0900

    [BP-85] fetch commits api (#29)
    * fetch commits api
    
    * fix include model error
    
    * variable rename

--- src/_models.py
+++ src/_models.py
@@ -14,6 +14,7 @@ class BaseModel(Base):
         exclude_null: bool = False,
         exclude_field: set[str] = set(),
         exclude_id: bool = False,
+        include_field: set[str] = set(),
     ) -> dict[str, object]:
         """
         Convert model attributes to a dictionary.
@@ -26,12 +27,23 @@ class BaseModel(Base):
             Set of field names to exclude from the dictionary. Default is empty set.
         exclude_id : bool, optional
             If True, exclude the id field from the dictionary. Default is False.
+        include_field : set[str], optional
+            Set of field names to include from the dictionary. Default is empty set.
+            Cannot be used if some exclude options are used.
 
         Returns
         -------
         dict
             A dictionary representation of the model's attributes.
         """
+        if include_field:
+            if exclude_field or exclude_id or exclude_null:
+                raise Exception("cannot select include_field with exclude options")
+            return {
+                c.key: getattr(self, c.key)
+                for c in inspect(self).mapper.column_attrs
+                if c.key in include_field
+            }
         return {
             c.key: getattr(self, c.key)
             for c in inspect(self).mapper.column_attrs
